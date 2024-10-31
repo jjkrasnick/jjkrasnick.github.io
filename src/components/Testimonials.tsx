5cf76791c8e8d0b6c7205d26ae6c4309f3d562f8
JJ Krasnick <jjkrasnick23@gmail.com>
2024-10-31 13:49:48 -0700

    Delete src/components/Testimonials.tsx

--- src/components/Testimonials.tsx
+++
@@ -1,6 +0,0 @@
-<diff>@@ -48,1 +48,1 @@
--              <p className="text-gray-700 dark:text-gray-300 mb-6 italic">"{testimonial.text}"</p>
-+              <p className="text-black dark:text-white mb-6 italic">"{testimonial.text}"</p>
-@@ -57,1 +57,1 @@
--                  <p className="text-sm text-gray-600 dark:text-gray-400">{testimonial.role}</p>
-+                  <p className="text-sm text-black dark:text-white">{testimonial.role}</p></diff>
